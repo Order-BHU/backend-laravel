d811fb50a0a83c26dcbee3f53aa75b7b51c0062b
Daniel <danieloluwasegun1000@gmail.com>
2025-02-05 04:02:25 +0100

    driver

--- app/Http/Controllers/User/OrderController.php
+++ app/Http/Controllers/User/OrderController.php
@@ -296,6 +296,7 @@ class OrderController extends Controller
                 // Get all drivers with no orders
                 $driversWithNoOrders = array_diff($drivers->pluck('id')->toArray(), Order::pluck('driver_id')->toArray());
 
+                return $driversWithNoOrders;
                 // If all drivers have orders, find driver with least number of orders
                 if (empty($driversWithNoOrders)) {
                     // Finds driver with least Orders

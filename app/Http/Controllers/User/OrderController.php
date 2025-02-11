a8e3e088636afe06cd81c1ef356642e27d490731
Daniel <danieloluwasegun1000@gmail.com>
2025-02-11 09:44:34 +0100

    yea

--- app/Http/Controllers/User/OrderController.php
+++ app/Http/Controllers/User/OrderController.php
@@ -36,7 +36,7 @@ class OrderController extends Controller
                 'items' => $request->items,
                 'restaurant_id' => $restaurantId,
                 'total' => $request->total,
-                'customer_location' => $request->location,
+                'customer_location' => "Bingham St",
                 'status' => 'pending',
                 'order_code' => $randomCode,
             ]);

fad26a9a273ca564214c30dd06bcb23b5bc7b395
Daniel <danieloluwasegun1000@gmail.com>
2025-02-16 22:00:46 +0100

    pay

--- app/Http/Controllers/User/OrderController.php
+++ app/Http/Controllers/User/OrderController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\User;
 
-use illumiate\Support\Facades\Http;
+use Illuminate\Support\Facades\Http;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Restaurant;

79361cc4b6c83927bddc1b96537c64b00f010e02
Daniel <danieloluwasegun1000@gmail.com>
2025-03-18 23:44:28 +0100

    track-order

--- app/Models/Transactions.php
+++ app/Models/Transactions.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Transactions extends Model
 {
     protected $table = 'transactions';
+
+    protected $fillable = [
+        'customer_id',
+        'restaurant_id',
+        'amount',
+        'type',
+        'status',
+        'reference',
+    ];
 }

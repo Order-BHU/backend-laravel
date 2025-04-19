a2d2f7a24e11c89fef20633fbf9cae93adf29421
Daniel <danieloluwasegun1000@gmail.com>
2025-04-19 23:19:19 +0100

    got-it

--- app/Models/Driver.php
+++ app/Models/Driver.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Driver extends Model
 {
-    //
+    protected $table = 'drivers';
+
+    protected $fillable = [
+       'user_id',
+       'receipient_code',
+    ];
 }
--- app/Models/DriverTransfers.php
+++ app/Models/DriverTransfers.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class DriverTransfers extends Model
 {
-    //
+    protected $table = 'driver_transfers';
+
+    protected $fillable = [
+      'user_id',
+      'status',
+      'reference',
+      'transfer_code'
+
+    ];
 }

38f9903ad33539e08006cccddd6e13a529f6432c
Daniel <danieloluwasegun1000@gmail.com>
2025-06-10 07:38:47 +0100

    paymentController

--- app/Http/Controllers/User/RestaurantController.php
+++ app/Http/Controllers/User/RestaurantController.php
@@ -16,7 +16,7 @@ class RestaurantController extends Controller
     public function restaurantList()
     {
        $allRestaurants = Restaurant::select('id', 'name', 'logo')
-    ->whereIn('id', [14])
+    ->whereIn('id', [14,15])
     ->get();
 
         // Update the logo field to include the full URL path
